fd1f35b47c2729d05bc26af7658f9e8f271ddddc
Kumar Sandeep Ramanath <kumarsandeepr89@gmail.com>
2021-06-27 02:02:00 +0530

    code challenges

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -27,3 +27,166 @@ const restaurant = {
     },
   },
 };
+
+// Coding Challenge #2
+
+/* 
+Let's continue with our football betting app!
+
+1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+      Odd of victory Bayern Munich: 1.33
+      Odd of draw: 3.25
+      Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+
+BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, 
+and the number of goals as the value. In this game, it will look like this:
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+      }
+
+GOOD LUCK 😀
+*/
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+for (const [indx, scorer] of game.scored.entries()) {
+  console.log(`Goal ${indx + 1} : ${scorer}`);
+}
+
+let sum = 0;
+for (const odd of Object.values(game.odds)) {
+  sum += odd;
+}
+console.log(`Avg Sum : ${sum / Object.values(game.odds).length}`);
+
+for (const [key, value] of Object.entries(game.odds)) {
+  let teamName = game[key] === undefined ? 'draw' : game[key];
+  console.log(`Odd of ${teamName} : ${value} `);
+}
+
+let number = '1234567890';
+let visibleNumber = number.slice(-4);
+console.log(visibleNumber.padStart(number.length, '*'));
+
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+
+/*
+ */
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  let inputValues = text.split('\n');
+  //console.log(inputValues);
+  let capitalizedArray = [];
+  for (const word of inputValues) {
+    const [first, second] = word.trim().toLowerCase().split('_');
+    console.log(
+      `${first}${second.replace(second[0], second[0].toUpperCase())}`
+    );
+  }
+  //console.log(capitalizedArray);
+});
+
+/*
+///////////////////////////////////////
+// String Methods Practice
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+*/
+
+let flightsNew = flights.split('+');
+//console.log(flightsNew);
+for (const flight of flightsNew) {
+  //console.log(flight);
+  const [deptOrArr, fromLoc, toLoc, time] = flight.split(';');
+  console.log(
+    `${deptOrArr.slice(1).replace('_', ' ')} from ${fromLoc
+      .slice(0, 3)
+      .toUpperCase()} to ${toLoc.slice(0, 3).toUpperCase()} (${time.replace(
+      ':',
+      'h'
+    )})`
+  );
+}
